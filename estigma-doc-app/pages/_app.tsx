50fd8ed3db0091cabe11cf27f796d6677843f8dc
Milonguita <guta.flores@gmail.com>
2025-10-01 10:05:23 -0300

    Restore old project

--- estigma-doc-app/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
